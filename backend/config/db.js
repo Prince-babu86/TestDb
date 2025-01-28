885e71bbab55c74f37f80a7769af0699d6988d47
Prince Babu <princebabu4495@gmail.com>
2025-01-28 16:43:20 +0530

    env file change

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,9 @@ require("dotenv").config()
 
 const connectDb = async () => {
     let MONGODB_URI = process.env.MONGODB_URI
-   
     try {
-      await  mongoose.connect(`${MONGODB_URI}/TestDB`)
+      await  mongoose.connect(`${MONGODB_URI}/TestDb`)
         console.log("mongodb is connected")
-       
     } catch (error) {
         console.log(error.message)
     }
